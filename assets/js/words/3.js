17f4a834cb8a06506960e090039f280afcde39e9
kyeong-lee <83997620+kyeong-lee@users.noreply.github.com>
2022-12-11 16:17:49 +0900

    Create 3.js

---
+++ assets/js/words/3.js
@@ -0,0 +1,3 @@
+YBM김_4학년_1학기 = ["BED","BOX","CUT","DAD","FOR","HAT","LIE","SAD","SHE","SON","WHO"];
+YBM김_4학년_2학기 = ["ART","BAT","BUY","CUP","DAY","FAN","FUN","HOT","ICE","OFF","PIE","TRY","USE"];
+YBM김_4학년_전체 = YBM김_4학년_1학기.concat(YBM김_4학년_2학기);
