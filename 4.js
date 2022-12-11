ab4dcdc8304224c10c03f358d7472f573d97db2e
kyeong-lee <83997620+kyeong-lee@users.noreply.github.com>
2022-12-11 16:02:44 +0900

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,3 @@
+YBM김_4학년_1학기 = ["BABY","BACK","BALL","BUSY","COME","COOK","CUTE","DESK","DRUM","FAST","FIND","FORK","GLUE","KIND","KNOW","LOVE","MASK","NAME","PLAY","SURE","TALL","WAKE","WOLF"];
+YBM김_4학년_2학기 = ["BOWL","CAKE","COOL","DRAW","FISH","FULL","GOWN","LONG","MILK","READ","RICE","SOCK","SOME","SONG","SOUP","STAR","STOP","WAIT","WANT"];
+YBM김_4학년_전체 = YBM김_4학년_1학기.concat(YBM김_4학년_2학기);
